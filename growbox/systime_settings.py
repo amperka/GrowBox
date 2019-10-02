a5abc836e6ec74c89426a424c0876092a22587ae
AlexErAmp <43956118+AlexErAmp@users.noreply.github.com>
2019-10-02 12:20:52 +0300

    Delete try-except from set_systime function.

--- growbox/systime_settings.py
+++ growbox/systime_settings.py
@@ -3,7 +3,4 @@ import subprocess
 
 def set_systime(date_time):
     command = ["sudo", "date", "--set=" + date_time]
-    try:
-        subprocess.run(command, check=True)
-    except subprocess.CalledProcessError:
-        raise
+    subprocess.run(command, check=True)
